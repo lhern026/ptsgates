bc000a5726a0cb39fba0d5764b3280c3918ad833
luis hernandez <lhern026@gmail.com>
2024-06-06 10:39:59 -0700

    first

---
+++ config/site.ts
@@ -0,0 +1,58 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautifujjl websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Products",
+      href: "/products",
+    },
+    {
+      label: "Solutions",
+      href: "/solutions",
+    },
+    {
+      label: "CustomerSuccess",
+      href: "/customersuccess",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Products",
+      href: "/products",
+    },
+    {
+      label: "CustomerSuccess",
+      href: "/customersuccess",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Solutions",
+      href: "/solutions",
+    },
+    
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
