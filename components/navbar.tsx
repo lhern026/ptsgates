dc05821570727282e7a395a8833a71e54dcbc1fe
luis hernandez <lhern026@gmail.com>
2024-06-12 13:56:15 -0700

    yeah

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -143,7 +143,7 @@ export const Navbar = () => {
                   className="px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]"
                 >
                   <NextLink className=" " href={item.href}>
-                    {item.label}
+                    {item.label}d
                   </NextLink>
                 </motion.div>
               )}
