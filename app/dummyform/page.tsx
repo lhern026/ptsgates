4792717fc909f70a91e673db980041a03aa49152
luis hernandez <lhern026@gmail.com>
2024-06-20 00:45:53 -0700

    push

--- app/dummyform/page.tsx
+++ app/dummyform/page.tsx
@@ -1,33 +1,261 @@
 "use client";
-import ContactForm from "../../components/ContactForm-2";
-import Image from "next/image";
 
-const ContactPage: React.FC = () => (
-  <div className="min-h-screen flex items-center justify-center bg-gray-100 relative">
-    {/* Background Image */}
-    <div className="absolute inset-0 z-0">
-      <Image
-        src="/background.jpg" // Make sure to have a suitable background image in your public folder
-        alt="Background Image"
-        layout="fill"
-        objectFit="cover"
-        quality={80}
-        className="opacity-50"
-      />
-    </div>
+import React, { useState } from "react";
+import Link from "next/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { useForm, ValidationError } from "@formspree/react";
+import { motion } from "framer-motion";
+import ReCAPTCHA from "react-google-recaptcha";
 
-    {/* Content */}
-    <div className="relative z-10 max-w-3xl w-full p-8 bg-white bg-opacity-90 shadow-lg rounded-lg">
-      <h1 className="text-4xl font-bold text-center mb-6 text-gray-800">
-        Contact Us
-      </h1>
-      <p className="text-center text-lg mb-8 text-gray-600">
-        We'd love to hear from you! Please fill out the form below and we'll get
-        in touch as soon as possible.
-      </p>
-      <ContactForm />
-    </div>
-  </div>
-);
+const containerVariants = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.6, staggerChildren: 0.2 },
+  },
+};
 
-export default ContactPage;
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+};
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xvgpppye");
+  const [recaptchaToken, setRecaptchaToken] = useState<string | null>("");
+
+  const handleRecaptchaVerify = (token: string | null) => {
+    setRecaptchaToken(token);
+  };
+
+  const Card = () => (
+    <motion.div className="bg-white p-8 rounded-lg shadow-lg text-center w-full max-w-lg flex-1 mt-0">
+      <motion.h2
+        className="text-2xl font-extrabold mb-4 text-gray-800"
+        variants={itemVariants}
+      >
+        Contact Information
+      </motion.h2>
+
+      <motion.p
+        className="text-base text-gray-700 mb-2 font-medium"
+        variants={itemVariants}
+      >
+        <span className="text-lg font-bold">Address</span>
+      </motion.p>
+      <motion.p
+        className="text-base text-gray-700 font-medium"
+        variants={itemVariants}
+      >
+        16226 Suttles Drive
+      </motion.p>
+      <motion.p
+        className="text-base text-gray-700 font-medium"
+        variants={itemVariants}
+      >
+        Riverside, CA 92504
+      </motion.p>
+
+      <motion.p
+        className="text-base text-gray-700 mb-2 font-medium mt-6"
+        variants={itemVariants}
+      >
+        <span className="text-lg font-bold">Phone Numbers</span>
+      </motion.p>
+      <motion.p
+        className="text-base text-gray-700 font-medium"
+        variants={itemVariants}
+      >
+        Toll Free: 888-282-4506
+      </motion.p>
+      <motion.p
+        className="text-base text-gray-700 font-medium"
+        variants={itemVariants}
+      >
+        Main Office: (951) 840-4324
+      </motion.p>
+
+      <Link
+        href="/"
+        className={buttonStyles({
+          color: "primary",
+          radius: "full",
+          variant: "shadow",
+          className: "px-6 py-3 text-lg font-semibold mt-6",
+        })}
+      >
+        Go to Home
+      </Link>
+    </motion.div>
+  );
+
+  if (state.succeeded) {
+    return (
+      <motion.div
+        className="flex items-center justify-center h-screen bg-white p-8 rounded-lg shadow-md text-center max-w-lg mx-auto"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6 }}
+      >
+        <div>
+          <h2 className="text-2xl font-extrabold mb-4 text-gray-800">
+            Thank You!
+          </h2>
+          <p className="text-lg text-gray-700 mb-6 font-medium">
+            Your message has been successfully sent. We will get back to you
+            shortly.
+          </p>
+          <Link
+            href="/"
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+              className: "px-6 py-3 text-lg font-semibold",
+            })}
+          >
+            Go to Home
+          </Link>
+        </div>
+      </motion.div>
+    );
+  }
+
+  return (
+    <motion.div
+      className="flex flex-col lg:flex-row max-w-full justify-center items-center lg:items-start bg-gradient-to-r from-blue-400 via-blue-500 to-red-100 py-16 px-6 gap-8"
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+    >
+      <Card />
+      <motion.form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (recaptchaToken) {
+            handleSubmit(e);
+          } else {
+            alert("Please complete the reCAPTCHA verification");
+          }
+        }}
+        className="w-full max-w-lg p-9 bg-white rounded-lg shadow-lg flex-1"
+        variants={containerVariants}
+      >
+        <motion.h2
+          className="text-2xl font-extrabold mb-6 text-center text-gray-800"
+          variants={itemVariants}
+        >
+          Contact Us
+        </motion.h2>
+
+        <motion.div className="mb-4" variants={itemVariants}>
+          <label
+            htmlFor="name"
+            className="block text-gray-700 mb-2 font-medium"
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Your name"
+            required
+          />
+          <ValidationError
+            prefix="Name"
+            field="name"
+            errors={state.errors}
+            className="text-red-500 text-sm mt-2"
+          />
+        </motion.div>
+
+        <motion.div className="mb-4" variants={itemVariants}>
+          <label
+            htmlFor="email"
+            className="block text-gray-700 mb-2 font-medium"
+          >
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Your email"
+            required
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+            className="text-red-500 text-sm mt-2"
+          />
+        </motion.div>
+
+        <motion.div className="mb-4" variants={itemVariants}>
+          <label
+            htmlFor="phone"
+            className="block text-gray-700 mb-2 font-medium"
+          >
+            Phone Number
+          </label>
+          <input
+            id="phone"
+            type="tel"
+            name="phone"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Your phone number"
+            required
+          />
+          <ValidationError
+            prefix="Phone"
+            field="phone"
+            errors={state.errors}
+            className="text-red-500 text-sm mt-2"
+          />
+        </motion.div>
+
+        <motion.div className="mb-6" variants={itemVariants}>
+          <label
+            htmlFor="message"
+            className="block text-gray-700 mb-2 font-medium"
+          >
+            Services Needed:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Your message"
+            required
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+            className="text-red-500 text-sm mt-2"
+          />
+        </motion.div>
+
+        <ReCAPTCHA
+          sitekey="6Ld0Lv0pAAAAALbu0FnmCKuaZapY5CSWPAdvSIsR"
+          onChange={handleRecaptchaVerify}
+        />
+
+        <motion.button
+          type="submit"
+          disabled={state.submitting || !recaptchaToken}
+          className="w-full py-3 bg-primary text-white font-semibold rounded-lg shadow-md hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary mt-4"
+          variants={itemVariants}
+        >
+          Submit
+        </motion.button>
+      </motion.form>
+    </motion.div>
+  );
+}
+
+export default ContactForm;
